5060a7811be163a867945f12ed6d26c7122ce4a0
AnnabelleWg <annabelle19991115@icloud.com>
2024-02-22 12:38:22 -0500

    Add description of the general workflow (i.e. from create new file in RStudio, stage, commit to push it to GitHub)

--- Testing.R
+++ Testing.R
@@ -1,3 +1,9 @@
 print("This file was created within RStudio")
 
 print("And now it lives on Github")
+
+print("General overflow: we linked Rstudio to GitHub so that we can push and 
+      pull repositories from within Rstuio. To test this, we created a 
+      repository on GitHub, linked it with a new project within RStudio, created
+      a new file and then staged, committed and pushed the file to our GitHub
+      repository")
